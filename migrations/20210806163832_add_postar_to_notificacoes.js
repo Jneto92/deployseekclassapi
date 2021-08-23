f7d91adc747f551ea962039ce7c39b12a33ad194
Jneto92 <55671411+Jneto92@users.noreply.github.com>
2021-08-23 07:42:18 -0300

    update 011

--- migrations/20210806163832_add_postar_to_notificacoes.js
+++ migrations/20210806163832_add_postar_to_notificacoes.js
@@ -1,11 +1,11 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   knex.schema.table('notificacoes', function(table){
     table.boolean('postar').defaultTo(false)
   })
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   knex.schema.table('notificacoes', function(table){
       table.dropColumn('postar')
   })
