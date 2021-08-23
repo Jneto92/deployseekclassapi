1729ffb257547e5c7c58145e03c99c13b01d1c3a
Jneto92 <55671411+Jneto92@users.noreply.github.com>
2021-08-23 07:32:51 -0300

    update 008

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
     
       client: "mysql2",
       connection: {
-        host: "us-cdbr-east-04.cleardb.com/",
+        host: "us-cdbr-east-04.cleardb.com",
         database: "heroku_1b851355c2f4f69",
         user: "b385cf454a43e9",
         password: "0c5bc3d6"
